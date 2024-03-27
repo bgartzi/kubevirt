5bcfe123c6c94e24be535736f7b2181a9ed33050
Leonardo Milleri <lmilleri@redhat.com>
2024-03-27 06:38:12 -0400

    Make deviceInfo to be available to vdpa plugin
    Signed-off-by: Leonardo Milleri <lmilleri@redhat.com>

--- pkg/hooks/manager.go
+++ pkg/hooks/manager.go
@@ -228,6 +228,8 @@ func sortCallbacksPerHookPoint(callbacksPerHookPoint map[string][]*callBackClien
 }
 
 func (m *hookManager) OnDefineDomain(domainSpec *virtwrapApi.DomainSpec, vmi *v1.VirtualMachineInstance) (string, error) {
+	deviceInfo := os.Getenv("PCIDEVICE_KUBEVIRT_IO_SRIOV_NET_INFO")
+	log.Log.Infof("OnDefineDomain deviceInfo: %s", deviceInfo)
 	domainSpecXML, err := xml.MarshalIndent(domainSpec, "", "\t")
 	if err != nil {
 		return "", fmt.Errorf("Failed to marshal domain spec: %v", domainSpec)
@@ -244,7 +246,7 @@ func (m *hookManager) OnDefineDomain(domainSpec *virtwrapApi.DomainSpec, vmi *v1
 	}
 
 	for _, callback := range callbacks {
-		domainSpecXML, err = m.onDefineDomainCallback(callback, domainSpecXML, vmiJSON)
+		domainSpecXML, err = m.onDefineDomainCallback(callback, domainSpecXML, vmiJSON, deviceInfo)
 		if err != nil {
 			return "", err
 		}
@@ -253,7 +255,7 @@ func (m *hookManager) OnDefineDomain(domainSpec *virtwrapApi.DomainSpec, vmi *v1
 	return string(domainSpecXML), nil
 }
 
-func (m *hookManager) onDefineDomainCallback(callback *callBackClient, domainSpecXML, vmiJSON []byte) ([]byte, error) {
+func (m *hookManager) onDefineDomainCallback(callback *callBackClient, domainSpecXML, vmiJSON []byte, deviceInfo string) ([]byte, error) {
 	conn, err := grpcutil.DialSocketWithTimeout(callback.SocketPath, 1)
 	if err != nil {
 		log.Log.Reason(err).Errorf(dialSockErr, callback.SocketPath)
@@ -268,8 +270,9 @@ func (m *hookManager) onDefineDomainCallback(callback *callBackClient, domainSpe
 	case hooksV1alpha1.Version:
 		client := hooksV1alpha1.NewCallbacksClient(conn)
 		result, err := client.OnDefineDomain(ctx, &hooksV1alpha1.OnDefineDomainParams{
-			DomainXML: domainSpecXML,
-			Vmi:       vmiJSON,
+			DomainXML:  domainSpecXML,
+			Vmi:        vmiJSON,
+			DeviceInfo: deviceInfo,
 		})
 		if err != nil {
 			log.Log.Reason(err).Error("Failed to call OnDefineDomain")
@@ -279,8 +282,9 @@ func (m *hookManager) onDefineDomainCallback(callback *callBackClient, domainSpe
 	case hooksV1alpha2.Version:
 		client := hooksV1alpha2.NewCallbacksClient(conn)
 		result, err := client.OnDefineDomain(ctx, &hooksV1alpha2.OnDefineDomainParams{
-			DomainXML: domainSpecXML,
-			Vmi:       vmiJSON,
+			DomainXML:  domainSpecXML,
+			Vmi:        vmiJSON,
+			DeviceInfo: deviceInfo,
 		})
 		if err != nil {
 			log.Log.Reason(err).Error("Failed to call OnDefineDomain")
--- pkg/virt-handler/isolation/generated_mock_detector.go
+++ pkg/virt-handler/isolation/generated_mock_detector.go
@@ -40,6 +40,31 @@ func (m *MockPodIsolationDetector) EXPECT() *MockPodIsolationDetectorMockRecorde
 	return m.recorder
 }
 
+func (_mr *MockPodIsolationDetector) Detect(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "Detect", arg0)
+}
+
+func (_m *MockPodIsolationDetector) DetectForSocket(vm *v1.VirtualMachineInstance, socket string) (IsolationResult, error) {
+	ret := _m.ctrl.Call(_m, "DetectForSocket", vm, socket)
+	ret0, _ := ret[0].(IsolationResult)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+func (_mr *MockPodIsolationDetector) DetectForSocket(arg0, arg1 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "DetectForSocket", arg0, arg1)
+}
+
+func (_m *MockPodIsolationDetector) Allowlist(controller []string) PodIsolationDetector {
+	ret := _m.ctrl.Call(_m, "Allowlist", controller)
+	ret0, _ := ret[0].(PodIsolationDetector)
+	return ret0
+}
+
+func (_mr *MockPodIsolationDetector) Allowlist(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "Allowlist", arg0)
+}
+
 // AdjustResources mocks base method.
 func (m *MockPodIsolationDetector) AdjustResources(vm *v1.VirtualMachineInstance, additionalOverheadRatio *string) error {
 	m.ctrl.T.Helper()
